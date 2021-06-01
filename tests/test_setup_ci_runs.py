be669618eabbeeb25094c0150f9fb1f0a534e5f7
Blake <yourlocalblake@gmail.com>
2021-06-01 18:46:38 +1000

    add initial tests funcs

---
+++ tests/test_setup_ci_runs.py
@@ -0,0 +1,10 @@
+import pytest
+
+from tidal_stability.utils import *
+from tidal_stability.data_formats import *
+from tidal_stability.solve import *
+
+
+def test_trivial():
+    # This is simply to have a function which is called
+    return 1
