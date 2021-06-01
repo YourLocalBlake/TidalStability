fcea97e4890daf79a935c376f5f8f4534acb0d44
Blake <yourlocalblake@gmail.com>
2021-06-01 16:54:41 +1000

    remove scikits.odes

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setuptools.setup(
         "matplotlib>=3.3.2",
         "mpmath>=1.1.0",
         "numpy>=1.19.2",
-        "scikits.odes==2.6.1",
         "scipy>=1.5.2",
+        # todo: sundials -> allows scikits.
     ],
     #python_requires = '>=3.5',
     author = u"Blake Staples",
