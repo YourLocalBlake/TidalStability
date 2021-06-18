1350b7298f0b6d8662a9db2030387ff5f8e1255b
Blake <yourlocalblake@gmail.com>
2021-06-18 18:54:11 +1000

    fix CRLF vs LF end line problem for ensure script

--- ci/ensure_sundials_installed_CRLF.sh
+++ ci/ensure_sundials_installed_CRLF.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Reproduced from https://github.com/bmcage/odes/
+# Copyright (C) 2011-12  Pavol Kišon
+# Copyright (C) 2011-12  Benny Malengier
+# All rights reserved.
+# Reproduced under the new-BSD licence and slightly modified.
 SUNDIALS_DEFAULT_VERSION='5.1.0'
 SUNDIALS_DEFAULT_PRECISION='double'
 SUNDIALS_DEFAULT_INDEX_SIZE='64'
@@ -11,7 +16,7 @@ SUNDIALS_INCLUDEDIR=$SUNDIALS_DIR/include
 if [ ! -d "$SUNDIALS_LIBDIR" ]; then
     mkdir -p $SUNDIALS_DIR
     echo "Installing sundials ${SUNDIALS_VERSION:-$SUNDIALS_DEFAULT_VERSION}"
-    ./ci_support/install_sundials.sh
+    ./ci/install_sundials.sh
 else
     echo "Using cached sundials in $SUNDIALS_LIBDIR"
 fi
